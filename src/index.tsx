fd208022fddbeac55f9a78d7fdee945a07af289f
Kristina Miakaeva <92623612+kmiakaeva@users.noreply.github.com>
2023-05-12 17:36:58 +0300

    add saving the cart to the local storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,48 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import filter from './slices/filterSlice';
 import search from './slices/searchSlice';
 import cart from './slices/cartSlice';
 import pizza from './slices/pizzaSlice';
 
-export const store = configureStore({
-  reducer: {
-    filter,
-    search,
-    cart,
-    pizza,
-  },
+const rootReducer = combineReducers({
+  filter,
+  search,
+  cart,
+  pizza,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
